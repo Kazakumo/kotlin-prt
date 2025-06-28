296ec2520a9a55973449a2817d8d8766c4a007ad
Kazakumo <shunya.sato1402@gmail.com>
2025-06-28 20:11:00 +0900

    feat: プロジェクトの基礎的実装

---
+++ src/main/kotlin/jp/satoshisato1402/kotlin_prt/KotlinPrtApplication.kt
@@ -0,0 +1,11 @@
+package jp.satoshisato1402.kotlin_prt
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinPrtApplication
+
+fun main(args: Array<String>) {
+	runApplication<KotlinPrtApplication>(*args)
+}
